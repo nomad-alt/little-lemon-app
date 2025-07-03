8cbc92b9dd2db0f788640fde84ce5627329e73fa
nomad-alt <nomad-alt@proton.me>
2025-07-03 15:37:59 +0200

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import HomePage from './components/HomePage'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,18 @@
+import Hero from './Hero'
+import Specials from './Specials'
+import Testimonials from './Testimonials'
+import About from './About'
+
+
+const HomePage = () => {
+  return (
+    <main>
+        <Hero />
+        <Specials />
+        <Testimonials />
+        <About />
+    </main>
+  )
+}
+
+export default HomePage
